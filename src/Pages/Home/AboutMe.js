6464bc02fc51c97b1bb56fb83f0b8bf9d02a40ea
Md Hasibul Islam <hasiee8004@gmail.com>
2022-07-12 12:13:11 +0600

    bug fixes

--- src/Pages/Home/AboutMe.js
+++ src/Pages/Home/AboutMe.js
@@ -6,160 +6,172 @@ import "./AboutMe.css";
 import img from "./hasib.me.jpg";
 
 const AboutMe = () => {
-
   return (
     <div className="about-me-section">
-
-<h3 className="posts-top">আমার সম্পর্কে </h3> <hr />
-    <div className="about">
+      <h3 className="posts-top">আমার সম্পর্কে </h3> <hr />
+      <div className="about">
         <div className="section">
-            <img src={img} alt="hasibul islam" /> <br />
-            <div className="social-icons">
-                    <li> <a target="_blank" href="https://www.facebook.com/Hasib.Rockie/">Facebook</a> </li>
-                    <li> <a target="_blank" href="https://md-hasibul-islam.netlify.app/">Website</a> </li>
-                    <li> <a target="_blank" href="https://www.youtube.com/channel/UCqXxlhr3YQ_y-byyO0oGS7A">Youtube</a> </li>
-                    <li> <a target="_blank" href="https://github.com/HasibRockie">Github</a> </li>
-                    <li> <a target="_blank" href="https://www.linkedin.com/in/hasib009/">Linkedin</a> </li>
-                    <li> <a target="_blank" href="mailto:hasibrockie@yahoo.com">E-Mail</a> </li> 
-                            
-            </div>
+          <img src={img} alt="hasibul islam" /> <br />
+          <div className="social-icons">
+            <li>
+              {" "}
+              <a target="_blank" href="https://www.facebook.com/Hasib.Rockie/">
+                <i className="fab fa-facebook-f"></i>
+              </a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a target="_blank" href="https://md-hasibul-islam.netlify.app/">
+              <i class="fas fa-globe"></i>
+              </a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a
+                target="_blank"
+                href="https://www.youtube.com/channel/UCqXxlhr3YQ_y-byyO0oGS7A"
+              >
+                <i class="fab fa-youtube"></i>
+              </a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a target="_blank" href="https://github.com/HasibRockie">
+                <i className="fab fa-github"></i>
+              </a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a target="_blank" href="https://www.linkedin.com/in/hasib009/">
+                <i className="fab fa-linkedin"></i>
+              </a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a target="_blank" href="mailto:hasibrockie@yahoo.com">
+              <i class="fas fa-envelope"></i>
+              </a>{" "}
+            </li>
+          </div>
         </div>
         <div className="section">
-            <div className="about-details">
-                <div className="que-items">
-                    <h4 className="que">নামঃ </h4>
-                    <h4 className="ans">মোঃ হাসিবুল ইসলাম</h4> 
-                </div>
-                <div className="que-items">
-                    <h4 className="que">জেলাঃ</h4>
-                    <h4 className="ans">লক্ষ্মীপুর</h4>
-                </div>
-                <div className="que-items">
-                    <h4 className="que">জন্মদিনঃ</h4>
-                    <h4 className="ans">২৯ জুন</h4>
-                </div>
-                <div className="que-items">
-                    <h4 className="que">ব্লাড গ্রুপঃ </h4>
-                    <h4 className="ans">ও-পজেটিভ</h4>
-                </div>
-                <div className="que-items">
-                    <h4 className="que">প্রাথমিক বিদ্যালয়ঃ</h4>
-                    <h4 className="ans">লক্ষ্মীপুর পি,টি,আই, সংলগ্ন পরীক্ষণ বিদ্যালয়, লক্ষ্মীপুর। </h4>
-                </div>
-                <div className="que-items">
-                    <h4 className="que">মাধ্যমিক বিদ্যালয়ঃ</h4>
-                    <h4 className="ans">লক্ষ্মীপুর আদর্শ সামাদ সরকারি উচ্চ বিদ্যালয় </h4>
-                </div>
-                <div className="que-items">
-                    <h4 className="que">উচ্চ-মাধ্যমিক বিদ্যালয়ঃ</h4>
-                    <h4 className="ans">বীরশ্রেষ্ঠ মুন্সী আব্দুর রউফ পাবলিক কলেজ, পিলখানা, ঢাকা। </h4>
-                </div>
-                <div className="que-items">
-                    <h4 className="que">বিশ্ববিদ্যালয়ঃ </h4>
-                    <h4 className="ans">বাংলাদেশ প্রকৌশল বিশ্ববিদ্যালয় (বুয়েট), ঢাকা। </h4>
-                </div>
-                <div className="que-items">
-                    <h4 className="que">বিভাগঃ </h4>
-                    <h4 className="ans">যন্ত্রকৌশল (মেকানিক্যাল ইঞ্জিনিয়ারিং) </h4>
-                </div>
-                <div className="que-items">
-                    <h4 className="que">দক্ষতাঃ</h4>
-                    <h4 className="ans">
-                        <ul>
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>JAVASCRIPT</li>
-                            <li>PYTHON</li>
-                            <li>C PROGRAMMING</li>
-                            <li>EXPRESS JS</li>
-                            <li>MONGO DB</li>
-                            <li>REACT JS</li>
-                            <li>FIREBASE</li>
-                            <li>DJANGO</li>
-                            <li>WORDPRESS</li>
-                            <li>MATLAB</li>
-                            <li>ARDUINO </li>
-                            <li>AUTOCAD</li>
-                            <li>SOLIDWORKS</li>
-                            <li>CAMTASIA</li>
-                            <li>MICROSOFT WORD, EXCEL, POWERPOINT </li>
-                        </ul>
-                    </h4>
-                </div>
-
-                <div className="que-items">
-                    <h4 className="que">অর্জন/সার্টিফিকেশনঃ </h4>
-                    <h4 className="ans">
-                        <ul>
-                            <li>প্রেসিডেন্ট'স স্কাউট অ্যাওয়ার্ড (পিএস ২০১৫) </li>
-                            <li>উপজেলা শ্রেষ্ঠ স্কাউট (২০১৬) </li>
-                            <li>সৃজনশীল মেধা অন্বেষণ (উপজেলা চ্যাম্পিয়ন ২০১৪ ও ২০১৫) </li>
-                            <li>সৃজনশীল মেধা অন্বেষণ (জেলা চ্যাম্পিয়ন ২০১৪)</li>
-                            <li>CERTIFICATE FOR COMBINED 1ST IN COLLEGE RESULT (BMARPC, 2017)</li>
-                            <li>পাইথন উইথ জ্যাঙ্গো (ক্রিয়েটিভ আইটি, ২০১৯) </li>
-                            <li>ফুল স্ট্যাক ওয়েব ডেভেলপমেন্ট (২০২১) </li>
-                            <li>রেসপন্সিভ ওয়েবসাইট ডিজাইন (ফ্রি-কোডক্যাম্প, ২০২১) </li>
-                            <li>ম্যাটল্যাব অনর‍্যাম্প (২০২১) </li>
-                            <li>ASSISTANT SECRETARY, WEB PORTAL (বুয়েট সাংবাদিক সমিতি, ২০২১) </li>
-                            <li>টিভি নিউজ প্রেজেন্টেশন (বিজেম, ২০২২) </li>
-                            <li>জুডো ও কারাতে (হলুদ বেল্ট, ২০২২) </li>
-                            <li>আরবি ভাষা (আভাই, ঢাবি, ২০২২) </li>
-                        </ul>
-                    </h4>
-                </div>
-
-                <div className="que-items">
-                    <h4 className="que">আগ্রহ/শখঃ </h4>
-                    <h4 className="ans">
-                        <ul>
-                            <li>ভ্রমণ </li>
-                            <li> ওয়েব ডেভেলপমেন্ট </li>
-                            <li> প্রোগ্রামিং </li>
-                            <li>কোয়ান্টাম মেকানিক্স </li>
-                            <li>ক্রিপ্টোগ্রাফি, কোড এন্ড সাইফার, এনক্রিপশন </li>
-                            <li>টাইম ট্রাভেল, টাইম ডায়ালেশন, টুইন প্যারাডক্স </li>
-                            <li>সাইকোলজি এবং পারসোনালিটি ভ্যারিয়েশন </li>
-                            <li>মেশিন লার্নিং এবং আর্টিফিসিয়াল ইন্টেলিজেন্স </li> 
-                            <li>ইসলামি ইতিহাস এবং ঐতিহ্য </li>
-                        </ul>
-                    </h4>
-                </div>
+          <div className="about-details">
+            <div className="que-items">
+              <h4 className="que">নামঃ </h4>
+              <h4 className="ans">মোঃ হাসিবুল ইসলাম</h4>
             </div>
+            <div className="que-items">
+              <h4 className="que">জেলাঃ</h4>
+              <h4 className="ans">লক্ষ্মীপুর</h4>
+            </div>
+            <div className="que-items">
+              <h4 className="que">জন্মদিনঃ</h4>
+              <h4 className="ans">২৯ জুন</h4>
+            </div>
+            <div className="que-items">
+              <h4 className="que">ব্লাড গ্রুপঃ </h4>
+              <h4 className="ans">ও-পজেটিভ</h4>
+            </div>
+            <div className="que-items">
+              <h4 className="que">প্রাথমিক বিদ্যালয়ঃ</h4>
+              <h4 className="ans">
+                লক্ষ্মীপুর পি,টি,আই, সংলগ্ন পরীক্ষণ বিদ্যালয়, লক্ষ্মীপুর।{" "}
+              </h4>
+            </div>
+            <div className="que-items">
+              <h4 className="que">মাধ্যমিক বিদ্যালয়ঃ</h4>
+              <h4 className="ans">
+                লক্ষ্মীপুর আদর্শ সামাদ সরকারি উচ্চ বিদ্যালয়{" "}
+              </h4>
+            </div>
+            <div className="que-items">
+              <h4 className="que">উচ্চ-মাধ্যমিক বিদ্যালয়ঃ</h4>
+              <h4 className="ans">
+                বীরশ্রেষ্ঠ মুন্সী আব্দুর রউফ পাবলিক কলেজ, পিলখানা, ঢাকা।{" "}
+              </h4>
+            </div>
+            <div className="que-items">
+              <h4 className="que">বিশ্ববিদ্যালয়ঃ </h4>
+              <h4 className="ans">
+                বাংলাদেশ প্রকৌশল বিশ্ববিদ্যালয় (বুয়েট), ঢাকা।{" "}
+              </h4>
+            </div>
+            <div className="que-items">
+              <h4 className="que">বিভাগঃ </h4>
+              <h4 className="ans">যন্ত্রকৌশল (মেকানিক্যাল ইঞ্জিনিয়ারিং) </h4>
+            </div>
+            <div className="que-items">
+              <h4 className="que">দক্ষতাঃ</h4>
+              <h4 className="ans">
+                <ul>
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>JAVASCRIPT</li>
+                  <li>PYTHON</li>
+                  <li>C PROGRAMMING</li>
+                  <li>EXPRESS JS</li>
+                  <li>MONGO DB</li>
+                  <li>REACT JS</li>
+                  <li>FIREBASE</li>
+                  <li>DJANGO</li>
+                  <li>WORDPRESS</li>
+                  <li>MATLAB</li>
+                  <li>ARDUINO </li>
+                  <li>AUTOCAD</li>
+                  <li>SOLIDWORKS</li>
+                  <li>CAMTASIA</li>
+                  <li>MICROSOFT WORD, EXCEL, POWERPOINT </li>
+                </ul>
+              </h4>
+            </div>
+
+            <div className="que-items">
+              <h4 className="que">অর্জন/সার্টিফিকেশনঃ </h4>
+              <h4 className="ans">
+                <ul>
+                  <li>প্রেসিডেন্ট'স স্কাউট অ্যাওয়ার্ড (পিএস ২০১৫) </li>
+                  <li>উপজেলা শ্রেষ্ঠ স্কাউট (২০১৬) </li>
+                  <li>সৃজনশীল মেধা অন্বেষণ (উপজেলা চ্যাম্পিয়ন ২০১৪ ও ২০১৫) </li>
+                  <li>সৃজনশীল মেধা অন্বেষণ (জেলা চ্যাম্পিয়ন ২০১৪)</li>
+                  <li>
+                    CERTIFICATE FOR COMBINED 1ST IN COLLEGE RESULT (BMARPC,
+                    2017)
+                  </li>
+                  <li>পাইথন উইথ জ্যাঙ্গো (ক্রিয়েটিভ আইটি, ২০১৯) </li>
+                  <li>ফুল স্ট্যাক ওয়েব ডেভেলপমেন্ট (২০২১) </li>
+                  <li>রেসপন্সিভ ওয়েবসাইট ডিজাইন (ফ্রি-কোডক্যাম্প, ২০২১) </li>
+                  <li>ম্যাটল্যাব অনর‍্যাম্প (২০২১) </li>
+                  <li>
+                    ASSISTANT SECRETARY, WEB PORTAL (বুয়েট সাংবাদিক সমিতি, ২০২১){" "}
+                  </li>
+                  <li>টিভি নিউজ প্রেজেন্টেশন (বিজেম, ২০২২) </li>
+                  <li>জুডো ও কারাতে (হলুদ বেল্ট, ২০২২) </li>
+                  <li>আরবি ভাষা (আভাই, ঢাবি, ২০২২) </li>
+                </ul>
+              </h4>
+            </div>
+
+            <div className="que-items">
+              <h4 className="que">আগ্রহ/শখঃ </h4>
+              <h4 className="ans">
+                <ul>
+                  <li>ভ্রমণ </li>
+                  <li> ওয়েব ডেভেলপমেন্ট </li>
+                  <li> প্রোগ্রামিং </li>
+                  <li>কোয়ান্টাম মেকানিক্স </li>
+                  <li>ক্রিপ্টোগ্রাফি, কোড এন্ড সাইফার, এনক্রিপশন </li>
+                  <li>টাইম ট্রাভেল, টাইম ডায়ালেশন, টুইন প্যারাডক্স </li>
+                  <li>সাইকোলজি এবং পারসোনালিটি ভ্যারিয়েশন </li>
+                  <li>মেশিন লার্নিং এবং আর্টিফিসিয়াল ইন্টেলিজেন্স </li>
+                  <li>ইসলামি ইতিহাস এবং ঐতিহ্য </li>
+                </ul>
+              </h4>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-
-</div>
-
   );
 };
 
 export default AboutMe;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
